88e7323f87139254544fbe00078830fa945f3957
Stephane Koman <stephane.kosodia@gmail.com>
2022-01-30 13:59:39 +0100

    Redirect back app to front

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+Route::fallback(function () {
+    return Redirect::away('http://degue-app-front.herokuapp.com');
 });
