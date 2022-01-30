7a4475710115b0eabd28f5dbb18142cbde11f579
Stephane Koman <stephane.kosodia@gmail.com>
2022-01-30 01:52:45 +0100

    Change project folders names

--- frontend/src/pages/dashborad/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-
-const Dashboard = () => {
-  const { t } = useTranslation();
-  return <div>{t('common.welcome')}</div>;
-};
-
-export default Dashboard;
--- frontend/src/utils/authHelpers.ts
+++
@@ -1,5 +0,0 @@
-import { JWT_TOKEN } from "./constantHelpers";
-
-export const getIsLoggedIn = (): boolean => {
-    return localStorage.getItem(JWT_TOKEN) ? true : false;
-}
